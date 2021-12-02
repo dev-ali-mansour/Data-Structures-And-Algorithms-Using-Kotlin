4a726020a2e6acd4e12d287a41fff18bc9e9e4c8
Ali Mansour <dev.ali.mansour@gmail.com>
2021-12-02 12:52:59 +0200

    Add Merge Sorted Arrays Solution

--- src/main/kotlin/data_structures/array/merge_sorted_array.kt
+++ src/main/kotlin/data_structures/array/merge_sorted_array.kt
@@ -1,15 +1,14 @@
 package data_structures.array
 
 fun mergeSortedArrays(array1: IntArray, array2: IntArray): IntArray {
-    val mergedArray = IntArray(array1.size + array2.size)
-    var i = 0
-    var j = 0
-    var k = 0
-
     // Check inputs
     if (array1.isEmpty()) return array2
     if (array2.isEmpty()) return array1
 
+    val mergedArray = IntArray(array1.size + array2.size)
+    var i = 0
+    var j = 0
+    var k = 0
     while (i < array1.size && j < array2.size) {
         if (array1[i] < array2[j]) {
             mergedArray[k] = array1[i]
