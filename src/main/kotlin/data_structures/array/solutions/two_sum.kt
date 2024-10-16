ddee16c5f2fbaa761e14e964765eb4927661c49b
Ali Mansour <dev.ali.mansour@gmail.com>
2024-10-16 05:55:33 +0300

    refactor: Create Two Solutions For Two Sum Problem. a Naive Solution And a Better One.

--- src/main/kotlin/data_structures/array/solutions/two_sum.kt
+++ src/main/kotlin/data_structures/array/solutions/two_sum.kt
@@ -1,24 +1,40 @@
 package data_structures.array.solutions
 
-import java.lang.IllegalArgumentException
-
 /* Problem:
  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
  */
 
+// Naive
+/*fun twoSum(nums: IntArray, target: Int): IntArray {
+    val result = mutableListOf<Int>()
+    for (i in nums.indices) {
+        for (j in nums.indices) {
+            if (nums[i] + nums[j] == target && i != j) {
+                if (!result.contains(i)) result.add(i)
+                if (!result.contains(j)) result.add(j)
+            }
+        }
+    }
+    return result.toIntArray()
+}*/
+
+// Better
 fun twoSum(nums: IntArray, target: Int): IntArray {
-    val map = HashMap<Int, Int>()
+    val map = mutableMapOf<Int, Int>()
     nums.forEachIndexed { index, item ->
-        val found = map[target - item]
-        found?.let { return intArrayOf(it, index) }
+        val complement = target - item
+        map[complement]?.let {
+            return intArrayOf(it, index)
+        }
         map[item] = index
     }
-    throw IllegalArgumentException("No Solution!")
+    return intArrayOf()
 }
 
 fun main() {
-    println(twoSum(intArrayOf(2, 7, 11, 15), 26).joinToString(", "))
-    println(twoSum(intArrayOf(3, 2, 4), 6).joinToString(", "))
+    println(twoSum(intArrayOf(2, 7, 11, 15), 9).contentToString())
+    println(twoSum(intArrayOf(3, 2, 4), 6).contentToString())
+    println(twoSum(intArrayOf(3, 3), 6).contentToString())
 }
